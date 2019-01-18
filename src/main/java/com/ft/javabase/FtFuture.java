0bc69a72328e56fe39b7af4f11513ef23e7189f6
feifa168 <2900093533@qq.com>
2019-01-18 14:23:12 +0800

    Future和FutureTask的使用

---
+++ src/main/java/com/ft/javabase/FtFuture.java
@@ -0,0 +1,103 @@
+package com.ft.javabase;
+
+import java.util.concurrent.*;
+
+interface FtCallBack<T, V> {
+    T call(Future<V> ft);
+}
+
+public class FtFuture {
+    public void futureByFutureTask(long millis, boolean block, FtCallBack<Boolean, Integer> callback, boolean threadMod) {
+        FutureTask<Integer> ft = new FutureTask<>(new Callable() {
+            @Override
+            public Integer call() throws Exception {
+                Thread.sleep(millis);
+                return 5;
+            }
+        });
+
+        if (threadMod) {
+            new Thread(ft).start();
+        } else {
+            ExecutorService es = Executors.newFixedThreadPool(1);
+            es.submit(ft);
+        }
+
+        if (!block) {
+            while (true) {
+                if (null != callback) {
+                    if (callback.call(ft)) {
+                        break;
+                    }
+                } else {
+                    if (!ft.isDone()) {
+                        System.out.println("future is not done");
+                    } else {
+                        System.out.println("--------future is done");
+                        break;
+                    }
+                }
+
+                try {
+                    Thread.sleep(millis / 2);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        try {
+            long begin = System.currentTimeMillis();
+            Integer result = (Integer) (ft.get());
+            long end = System.currentTimeMillis();
+            System.out.println("future with thread result is " + result + " total time is " + (end-begin));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void futureByFuture(long millis, boolean block, FtCallBack<Boolean, Integer> callback) {
+
+        ExecutorService es = Executors.newFixedThreadPool(1);
+        Future<Integer> ft = es.submit(()->{
+            Thread.sleep(millis);
+            return 5;
+        });
+
+        if (!block) {
+            while (true) {
+                if (null != callback) {
+                    if (callback.call(ft)) {
+                        break;
+                    }
+                } else {
+                    if (!ft.isDone()) {
+                        System.out.println("future is not done");
+                    } else {
+                        System.out.println("--------future is done");
+                        break;
+                    }
+                }
+
+                try {
+                    Thread.sleep(millis / 2);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        try {
+            long begin = System.currentTimeMillis();
+            Integer result = (Integer)(ft.get());
+            long end = System.currentTimeMillis();
+            System.out.println("future with thread result is " + result + " total time is " + (end-begin));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/test/java/com/ft/javabase/TestFtFuture.java
@@ -0,0 +1,88 @@
+package com.ft.javabase;
+
+import org.junit.Test;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.concurrent.Future;
+
+public class TestFtFuture {
+
+    static class FtCallBackImpl implements FtCallBack<Boolean, Integer> {
+        @Override
+        public Boolean call(Future<Integer> ft) {
+            Boolean done = ft.isDone();
+            if (!done) {
+                System.out.println("callback is not done");
+            } else {
+                System.out.println("========= callback is done");
+            }
+            return done;
+        }
+    }
+
+    private void testFuture(String methodName, long mills, boolean block, FtCallBack fcb) {
+        System.out.println("===========================================\nbegin " + methodName + " wait time["+mills+"ms] " + " block["+block+"] callback is null["+(fcb==null)+"]");
+        FtFuture fft = new FtFuture();
+        try {
+            Method md = fft.getClass().getDeclaredMethod(methodName, new Class[]{long.class, boolean.class, FtCallBack.class});
+            md.invoke(fft, mills, block, fcb);
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+        System.out.println("end " + methodName + " wait time["+mills+"ms] " + " block["+block+"] callback is null["+(fcb==null)  + "]");
+    }
+    private void testFutureTask(String methodName, long mills, boolean block, FtCallBack fcb, boolean threadMod) {
+        System.out.println("===========================================\nbegin " + methodName + " wait time["+mills+"ms] " + " block["+block+"] callback is null["+(fcb==null)+"] threadmod["+threadMod+"]");
+        FtFuture fft = new FtFuture();
+        try {
+            Method md = fft.getClass().getDeclaredMethod(methodName, new Class[]{long.class, boolean.class, FtCallBack.class, boolean.class});
+            md.invoke(fft, mills, block, fcb, threadMod);
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+        System.out.println("end " + methodName + " wait time["+mills+"ms] " + " block["+block+"] callback is null["+(fcb==null)  + "]");
+    }
+
+    public void testByFutureTask(boolean threadMod) {
+        FtCallBackImpl fcbi = new FtCallBackImpl();
+        long waitMills = 1000;
+        String methodName = "futureByFutureTask";
+        testFutureTask(methodName, waitMills, false, null, threadMod);
+        testFutureTask(methodName, waitMills, false, fcbi, threadMod);
+        testFutureTask(methodName, waitMills, true, null, threadMod);
+        testFutureTask(methodName, waitMills, true, fcbi, threadMod);
+    }
+    @Test
+    public void testByFutureTaskNotUseThread() {
+        testByFutureTask(true);
+    }
+    @Test
+    public void testByFutureTaskUseThread() {
+        testByFutureTask(false);
+    }
+    @Test
+    public void testByFutureTask() {
+        testByFutureTask(true);
+        testByFutureTask(false);
+    }
+
+    @Test
+    public void testByFuture() {
+        FtCallBackImpl fcbi = new FtCallBackImpl();
+        long waitMills = 1000;
+        String methodName = "futureByFuture";
+        testFuture(methodName, waitMills, false, null);
+        testFuture(methodName, waitMills, false, fcbi);
+        testFuture(methodName, waitMills, true, null);
+        testFuture(methodName, waitMills, true, fcbi);
+    }
+}
